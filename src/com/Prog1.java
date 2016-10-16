ecd14618f36160a8f6c99709e7c293aae87d6e60
snsinfotech <rajeevthetriner@gmail.com>
2016-10-16 12:22:14 +0530

    message

---
+++ src/com/Prog1.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 99;
+		int y = 100;
+		int c = x+y;
+		for (int i =0 ; i<7;i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println(c);
+
+	}
+
+}
